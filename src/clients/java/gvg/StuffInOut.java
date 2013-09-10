d9727e1fbfb824968a4613240b34805895727125
Julian Togelius <julian@togelius.com>
2013-09-10 14:29:11 +0200

    Clients directory structure

---
+++ src/clients/java/gvg/StuffInOut.java
@@ -0,0 +1,27 @@
+package gvg;
+
+import javax.xml.stream.util.StreamReaderDelegate;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.Buffer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: togelius
+ * Date: //130910
+ * Time: 14:19 PM
+ */
+public class StuffInOut {
+
+    public static void main(String[] args) throws Exception {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        while (true) {
+            String in = br.readLine();
+            System.out.println("Handfish");
+            System.err.println("Handfish" + in.charAt(0));
+        }
+    }
+
+}
