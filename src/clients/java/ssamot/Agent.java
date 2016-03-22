979694a27956e69e8781cdef668d3f70ea5bee26
diegopliebana <4TiMebos>
2016-03-22 11:43:13 +0000

    new agent

--- src/clients/java/ssamot/Agent.java
+++ src/clients/java/ssamot/Agent.java
@@ -17,67 +17,45 @@ import java.util.Random;
  */
 public class Agent extends AbstractPlayer {
 
-    /**
-     * Random generator for the agent.
-     */
-    private Random randomGenerator;
 
 
-    /**
-     * Public constructor with state observation and time due.
-     */
-    public Agent(StateObservation so, long timeDue)
-    {
-        randomGenerator = new Random();
+    public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+
     }
 
-
     /**
-     * Picks an action. This function is called every game step to request an
-     * action from the player.
+     *
+     * Very simple one step lookahead agent.
+     *
      * @param stateObs Observation of the current state.
-     * @param timeDue Time the action returned is due.
+     * @param elapsedTimer Timer when the action returned is due.
      * @return An action for the current state
      */
-    public Types.ACTIONS act(StateObservation stateObs, long timeDue) {
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
 
-        ArrayList<Vector2d>[] npcPositions = stateObs.getNPCPositions();
-        ArrayList<Vector2d>[] fixedPositions = stateObs.getImmovablePositions();
-        ArrayList<Vector2d>[] movingPositions = stateObs.getMovablePositions();
-        ArrayList<Vector2d>[] resourcesPositions = stateObs.getResourcesPositions();
-        ArrayList<Vector2d>[] portalPositions = stateObs.getPortalsPositions();
+        Types.ACTIONS bestAction = null;
+        double maxQ = Double.NEGATIVE_INFINITY;
+        SimpleStateHeuristic heuristic =  new SimpleStateHeuristic(stateObs);
+        for (Types.ACTIONS action : stateObs.getAvailableActions()) {
 
-        long remaining = timeDue - System.currentTimeMillis();
-        Types.ACTIONS action = null;
-        StateObservation stCopy = stateObs.copy();
-
-        int whenToFinish = 5;
-        /*if(stateObs.getGameTick() == 30)
-        {
-            System.out.println(stateObs.getGameTick() + ": let's take a nap.");
-            whenToFinish = -50;
-        } */     //To force a disqualification.
-
-        while(remaining > whenToFinish)
-        {
-            ArrayList<Types.ACTIONS> actions = stateObs.getAvailableActions();
-            int index = randomGenerator.nextInt(actions.size());
-            action = actions.get(index);
-
+            StateObservation stCopy = stateObs.copy();
             stCopy.advance(action);
-            if(stCopy.isGameOver())
-            {
-                //copying
-                //System.out.println("copying");
-                stCopy = stateObs.copy();
+            double Q = heuristic.evaluateState(stCopy);
+
+
+            //System.out.println("Action:" + action + " score:" + Q);
+            if (Q > maxQ) {
+                maxQ = Q;
+                bestAction = action;
             }
 
-            remaining = timeDue - System.currentTimeMillis();
-            //if(remaining<5)
-            //System.out.println(remaining);
+
         }
 
-        //System.out.println("Action to execute: " + action);
-        return action;
+        // System.out.println("====================");
+        return bestAction;
+
+
+
     }
 }
